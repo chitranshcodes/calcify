20ad4aed0812d92067fb663bbc02c33360662903
Chitransh <chitranshgaur@outlook.com>
2025-01-29 02:59:50 +0530

    added a picture

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image, ImageTk
 def digit(n):
     var.set(var.get()+n)
 def equal():
@@ -9,6 +10,7 @@ window.title("Calculator")
 window.configure(bg='light blue')
 var= StringVar()
 window.geometry('300x400')
+window.resizable(False,False)
 Label(window, text='Calcify',width=20, font="'Times' 16 bold"   ,fg='dark green', bg='yellow').grid(row=0, columnspan=3)
 Entry(window, textvariable=var).grid(row=1, columnspan=3)
 Button(window, text='7', width=14, command=lambda:digit('7')).grid(row=2,column=0)
@@ -27,5 +29,11 @@ Button(window, text='x', width=14, command=lambda:digit('*')).grid(row=6,column=
 Button(window, text='=', width=14, command=equal).grid(row=6,column=1)
 Button(window, text='/', width=14, command=lambda:digit('7')).grid(row=6,column=2)
 Button(window, text='clear', width=14, command=lambda:var.set("")).grid(row=7,column=1)
+img = Image.open(r"C:/Users/CHITRANSH GAUR/OneDrive/Desktop/Projects/tkinter-calci/calcify/hiladala.png")
+img = img.resize((280, 200))
+img = ImageTk.PhotoImage(img)
+
+Label(window, image=img).grid(row=8,columnspan=3, padx=0)
+
 
 window.mainloop()
